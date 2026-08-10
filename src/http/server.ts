b41ed09598a441f90e39520859bf51e6a86e3643
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-08-10 14:15:26 -0300

    feat: add type to request body in `[POST] /restaurants`

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,30 +1,41 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 
 import { env } from '../env';
 import { db } from '../db/connection';
 import { restaurants, users } from '../db/schema';
 
-const app = new Elysia().post('/restaurants', async ({ body, set }) => {
-  const { restaurantName, name, email, phone } = body as any;
+const app = new Elysia().post(
+  '/restaurants',
+  async ({ body, set }) => {
+    const { restaurantName, managerName, email, phone } = body;
 
-  const [manager] = await db
-    .insert(users)
-    .values({
-      name,
-      email,
-      phone,
-    })
-    .returning({
-      id: users.id,
+    const [manager] = await db
+      .insert(users)
+      .values({
+        name: managerName,
+        email,
+        phone,
+      })
+      .returning({
+        id: users.id,
+      });
+
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      managerId: manager?.id,
     });
 
-  await db.insert(restaurants).values({
-    name: restaurantName,
-    managerId: manager?.id,
-  });
-
-  set.status = 204;
-});
+    set.status = 204;
+  },
+  {
+    body: t.Object({
+      restaurantName: t.String(),
+      managerName: t.String(),
+      email: t.String(),
+      phone: t.String(),
+    }),
+  },
+);
 
 const PORT = env.PORT;
 
